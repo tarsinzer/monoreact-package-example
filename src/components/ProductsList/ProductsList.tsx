be96272c32b554cf32bdbd94f89309a857cc09a1
retarsis <tarsis.maksym@gmail.com>
2020-12-02 10:39:16 +0200

    fix: grommet typings

--- src/components/ProductsList/ProductsList.tsx
+++ src/components/ProductsList/ProductsList.tsx
@@ -4,13 +4,23 @@ import { Box, Grommet, grommet, List } from 'grommet';
 import { progress } from '../../shared/data';
 import styles from './ProductsList.scss';
 
-export const ProductsList = () => (
+export const ProductsList: React.FC = () => (
   <Grommet className={styles.container} theme={grommet}>
     <Box align='center' className={styles.list} pad='medium'>
       <List
         data={progress}
-        primaryKey={React.useCallback(item => item.name, [])}
-        secondaryKey={React.useCallback(item => `${item.percent}%`, [])}
+        primaryKey={React.useCallback(
+          (item: Product.Progress): React.ReactElement => (
+            <span>{item.name}</span>
+          ),
+          []
+        )}
+        secondaryKey={React.useCallback(
+          (item: Product.Progress): React.ReactElement => (
+            <span>{`${item.percent}%`}</span>
+          ),
+          []
+        )}
       />
     </Box>
   </Grommet>
